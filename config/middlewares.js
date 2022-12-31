eeea70f3d8da5e1c56ae3d220d12bd22ae7f680c
HaiDang309 <hada.nguyen309@gmail.com>
2022-12-31 12:50:11 +0700

    resolve cps blocking

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,35 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://storage.googleapis.com/uit-k14`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://storage.googleapis.com/uit-k14`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
